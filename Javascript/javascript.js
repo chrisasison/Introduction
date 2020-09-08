b051f6e430aae417965e7d68bb10a7f486ecc23e
Chris Sison <chris.a.sison@outlook.com>
2020-09-08 14:42:41 -0700

    Developing professional portfolio. Updated detailed about me section. Layout assembled with bootstrap 4. Link to Udemy JavaScript project.

---
+++ Javascript/javascript.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    console.log($(window).scrollTop());
+    $(window).scrollTop() >= 20 ? document.getElementById('navigation').classList.remove('bg-dark') : document.getElementById('navigation').classList.add('bg-dark');
+
+    if ($(window).scrollTop() >= 1048) {
+        document.getElementById('navigation').classList.remove('navbar-dark');
+        document.getElementById('navigation').classList.add('navbar-light');
+    } else {
+        document.getElementById('navigation').classList.add('navbar-dark');
+    }
+});
+
