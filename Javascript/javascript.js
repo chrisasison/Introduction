34821ecfdfe32c72160c71ad0d28ad4c93e688ef
Chris Sison <chris.a.sison@outlook.com>
2020-09-17 01:12:18 -0700

    updated positioning of UW logo

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,12 +1,20 @@
-window.addEventListener('scroll', function() {
-    console.log($(window).scrollTop());
+//navbar
+window.addEventListener('scroll', function(){
+    // console.log($(window).scrollTop());
     $(window).scrollTop() >= 20 ? document.getElementById('navigation').classList.remove('bg-dark') : document.getElementById('navigation').classList.add('bg-dark');
 
-    if ($(window).scrollTop() >= 1048) {
+    if ($(window).scrollTop() >= 1048){
         document.getElementById('navigation').classList.remove('navbar-dark');
         document.getElementById('navigation').classList.add('navbar-light');
-    } else {
+    } else{
         document.getElementById('navigation').classList.add('navbar-dark');
     }
 });
+
+//Udemy section
+
+//UW section
+
+
+
 
